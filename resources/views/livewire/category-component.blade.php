4c36ea6c67e75f0cdb11fafc5a8f9670b7468367
dbills1 <billyadam1018@gmail.com>
2021-10-12 10:36:36 +0700

    Creating for Product by Categories, adding CategoryComponent livewire and views

--- resources/views/livewire/category-component.blade.php
+++ resources/views/livewire/category-component.blade.php
@@ -3,8 +3,9 @@
     <div class="container">
         <div class="wrap-breadcrumb">
             <ul>
-                <li class="item-link"><a href="#" class="link">home</a></li>
-                <li class="item-link"><span>Digital & Electronics</span></li>
+                <li class="item-link"><a href="/" class="link">home</a></li>
+                <li class="item-link"><span>Product Categories</span></li>
+                <li class="item-link"><span>{{ $category_name }}</span></li>
             </ul>
         </div>
         <div class="row">
@@ -15,7 +16,7 @@
                     </a>
                 </div>
                 <div class="wrap-shop-control">
-                    <h1 class="shop-title">Digital & Electronics</h1>
+                    <h1 class="shop-title">{{ $category_name }}</h1>
                     <div class="wrap-right">
                         <div class="sort-item orderby ">
                             <select name="orderby" class="use-chosen" wire:model="sorting">
