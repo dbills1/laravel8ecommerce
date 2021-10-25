25d0eb1cf0f61beb982465a10c29ee7b6e4b4e07
dbills1 <billyadam1018@gmail.com>
2021-10-25 16:07:49 +0700

    Creating Admin Home Page Sliders dynamic and create funcion CRUD for the slider page

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,12 @@ use App\Http\Livewire\User\UserDashboardComponent;
 use App\Http\Livewire\Admin\AdminCategoryComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\Admin\AdminAddProductComponent;
+use App\Http\Livewire\Admin\AdminHomeSliderComponent;
 use App\Http\Livewire\Admin\AdminAddCategoryComponent;
 use App\Http\Livewire\Admin\AdminEditProductComponent;
 use App\Http\Livewire\Admin\AdminEditCategoryComponent;
+use App\Http\Livewire\Admin\AdminAddHomeSliderComponent;
+use App\Http\Livewire\Admin\AdminEditHomeSliderComponent;
 
 /*
 |--------------------------------------------------------------------------
@@ -60,4 +63,8 @@ Route::middleware(['auth:sanctum' , 'verified' , 'authadmin'])->group(function()
     Route::get('/admin/products', AdminProductComponent::class)->name('admin.products');
     Route::get('/admin/products/add', AdminAddProductComponent::class)->name('admin.addproduct');
     Route::get('/admin/products/edit/{product_slug}', AdminEditProductComponent::class)->name('admin.editproduct');
+
+    Route::get('/admin/slider', AdminHomeSliderComponent::class)->name('admin.homeslider');
+    Route::get('/admin/slider/add', AdminAddHomeSliderComponent::class)->name('admin.addhomeslider');
+    Route::get('/admin/slider/edit/{slide_id}', AdminEditHomeSliderComponent::class)->name('admin.edithomeslider');
 });
