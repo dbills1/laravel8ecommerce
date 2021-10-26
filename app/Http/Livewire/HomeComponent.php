c247f5cf05b08c0fa82bd3e15a4848915d30338e
dbills1 <billyadam1018@gmail.com>
2021-10-26 17:54:23 +0700

    Creating admin show latest product page in Homepage

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Livewire;
 
-use App\Models\HomeSlider;
+use App\Models\Product;
 use Livewire\Component;
+use App\Models\HomeSlider;
 
 class HomeComponent extends Component
 {
     public function render()
     {
         $sliders = HomeSlider::where('status',1)->get();
-        return view('livewire.home-component', ['sliders'=>$sliders])->layout('layouts.base');
+        $lproducts = Product::orderBy('created_at', 'DESC')->get()->take(8);
+        return view('livewire.home-component', ['sliders'=>$sliders, 'lproducts'=>$lproducts])->layout('layouts.base');
     }
 }
