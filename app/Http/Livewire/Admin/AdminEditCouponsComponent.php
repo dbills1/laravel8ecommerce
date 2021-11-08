423846ae333d2d80bb099268b122b7adf0e8d69f
dbills1 <billyadam1018@gmail.com>
2021-11-08 17:39:55 +0700

    feat(Admin\Coupons): Adding expiry date for coupon

--- app/Http/Livewire/Admin/AdminEditCouponsComponent.php
+++ app/Http/Livewire/Admin/AdminEditCouponsComponent.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire\Admin;
 
 use App\Models\Coupon;
 use Livewire\Component;
+use Illuminate\Validation\Rule;
 
 class AdminEditCouponsComponent extends Component
 {
@@ -12,6 +13,7 @@ class AdminEditCouponsComponent extends Component
     public $value;
     public $cart_value;
     public $coupon_id;
+    public $expiry_date;
 
     public function mount($coupon_id)
     {
@@ -21,31 +23,35 @@ class AdminEditCouponsComponent extends Component
         $this->value = $coupon->value;
         $this->cart_value = $coupon->cart_value;
         $this->coupon_id = $coupon->id;
+        $this->expiry_date = $coupon->expiry_date;
     }
 
     public function updated($fields)
     {
         $this->validateOnly($fields, [
-            'code' => 'required|unique:coupons',
+            'code' => ['required', Rule::unique('coupons')->ignore($this->coupon_id)],
             'type' => 'required',
             'value' => 'required|numeric',
-            'cart_value' => 'required|numeric'
+            'cart_value' => 'required|numeric',
+            'expiry_date' => 'required'
         ]);
     }
 
     public function updateCoupon()
     {
         $this->validate([
-            'code' => 'required|unique:coupons',
+            'code' => ['required', Rule::unique('coupons')->ignore($this->coupon_id)],
             'type' => 'required',
             'value' => 'required|numeric',
-            'cart_value' => 'required|numeric'
+            'cart_value' => 'required|numeric',
+            'expiry_date' => 'required'
         ]);
         $coupon = Coupon::find($this->coupon_id);
         $coupon->code = $this->code;
         $coupon->type = $this->type;
         $coupon->value = $this->value;
         $coupon->cart_value = $this->cart_value;
+        $coupon->expiry_date = $this->expiry_date;
         $coupon->save();
         session()->flash('message', 'Coupon has been Updated Successfully!');
     }
